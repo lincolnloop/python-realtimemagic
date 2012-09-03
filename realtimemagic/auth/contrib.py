d25b28a5a72329096c6054dffc3e4041e77ab9e8
Nicolas Lara <nicolaslara@gmail.com>
2012-09-03 14:14:53 +0200

    hack to handle cookies

--- realtimemagic/auth/contrib.py
+++ realtimemagic/auth/contrib.py
@@ -11,7 +11,14 @@ class DjangoAuthenticator(Authenticator):
 
     def get_user(self, conn):
         try:
+            logging.info(self.get_cookies(conn))
             session_key = self.get_cookies(conn).get('sessionid')
+            logging.info(session_key)
+            if 'Set-Cookie:' in session_key:
+                # Horrible hack
+                cookies = dict(i.split('=') for i in session_key.split('Set-Cookie:'))
+                session_key = cookies.get('sessionid')
+                logging.info('updating session key to %s' % session_key)
             session = Session.objects.get(session_key=session_key)
             uid = session.get_decoded().get('_auth_user_id')
             return User.objects.get(pk=uid)
