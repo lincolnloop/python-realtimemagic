6aecb0ff3ad0bffd5b765a99de55caf02d14c280
Yann Malet <yann.malet@gmail.com>
2012-09-07 17:41:25 +0200

    add support for user, database, pasword, host in PsqlMonitor in addition dsn

--- realtimemagic/monitors/psql.py
+++ realtimemagic/monitors/psql.py
@@ -6,14 +6,21 @@ from eventlet.hubs import trampoline
 
 
 class PsqlMonitor(object):
-    def __init__(self, dsn):
+    def __init__(self, dsn=None, database=None, user=None,
+            password=None, host=None, port=None):
         self.dsn = dsn
+        self.database = database
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
 
     def monitor(self, channel):
         def wrapper(f):
             @functools.wraps(f)
             def inner(pubsub):
-                cnn = psycopg2.connect(self.dsn)
+                cnn = psycopg2.connect(self.dsn, self.database, self.user,
+                        self.password, self.host, self.port)
                 cnn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
                 cur = cnn.cursor()
                 cur.execute("LISTEN %s;" % channel)
